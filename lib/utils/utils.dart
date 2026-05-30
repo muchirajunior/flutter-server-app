9e7feb38f3d7c99dab470410f13f4ac1165afd6f
MUCHIRAJUNIOR <muchirajunior022@gmail.com>
2026-05-30 17:28:04 +0300

    update connection function

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,7 +4,13 @@ import 'package:serverapp/utils/env.dart';
 
 class Utils {
 
-  static Future<Connection> get dbClient  async => await Connection.openFromUrl(Env.databaseURL);
+  static  Connection? _connection;
+
+  static Future<Connection> get dbClient  async {
+    if(_connection != null &&  _connection?.isOpen == true)return _connection!;
+    _connection =  await Connection.openFromUrl(Env.databaseURL);
+    return _connection!;
+  }
   
 }
 
